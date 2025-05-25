ff29ad8d74df3c0b0687822be157df65908cf165
j-naima0 <jannatulnaima519@gmail.com>
2025-05-24 21:41:18 -0400

    switch statement challenge activity

--- Guided and Challenge Project/Program.cs
+++ Guided and Challenge Project/Program.cs
@@ -51,7 +51,7 @@ Console.WriteLine((flip == 0) ? "heads" : "tails");
         {
             Console.WriteLine("Invalid input. Please enter a valid number.");
         }
-*/
+
 
 //Complete challenge activity using variable scope
 
@@ -70,3 +70,60 @@ if (found)
     Console.WriteLine("Set contains 42");
 
 Console.WriteLine($"Total: {total}");
+*/
+//switch statement challenge activity
+// SKU = Stock Keeping Unit
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
