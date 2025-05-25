53b48b7ad6ad9aa6b5d52438d4c393e38361ecf1
j-naima0 <jannatulnaima519@gmail.com>
2025-05-24 22:10:22 -0400

    Challenge Project 2
    code that validates string input

--- Guided and Challenge Project/Program.cs
+++ Guided and Challenge Project/Program.cs
@@ -163,7 +163,7 @@ do
 } while (hero > 0 && monster > 0);
 
 Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
-*/
+
 
 //challenge activity to differentiate between do and while iteration statements
 
@@ -202,4 +202,34 @@ do
 
 Console.WriteLine($"Your input value ({numValue}) has been accepted.");
 
+readResult = Console.ReadLine();
+
+*/
+
+//Cgallenge Project 2
+string? readResult;
+string roleName = "";
+bool validEntry = false;
+
+do
+{                
+    Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+    readResult = Console.ReadLine();
+    if (readResult != null) 
+    {
+        roleName = readResult.Trim();
+    }
+
+    if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+    {
+        validEntry = true;
+    }
+    else
+    {
+        Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+    }
+
+} while (validEntry == false);
+
+Console.WriteLine($"Your input value ({roleName}) has been accepted.");
 readResult = Console.ReadLine();
