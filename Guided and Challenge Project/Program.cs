98ff2eb18751e7ff8b93301f52df285c450fa128
j-naima0 <jannatulnaima519@gmail.com>
2025-05-24 21:39:32 -0400

    complete challenge activity using variable scope

--- Guided and Challenge Project/Program.cs
+++ Guided and Challenge Project/Program.cs
@@ -4,7 +4,7 @@
 Random coin = new Random();
 int flip = coin.Next(0, 2);
 Console.WriteLine((flip == 0) ? "heads" : "tails");
-*/
+
 
 // challenge activity using Boolean expressions
 
@@ -51,3 +51,22 @@ Console.WriteLine((flip == 0) ? "heads" : "tails");
         {
             Console.WriteLine("Invalid input. Please enter a valid number.");
         }
+*/
+
+//Complete challenge activity using variable scope
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
